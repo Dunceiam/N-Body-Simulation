82f9a6326e26b4655149ff4767c86625ead8339b
Dunceiam <dunceiam@gmail.com>
2010-12-29 00:53:58 -0500

    LOLWUT

--- src/Simulation2.cpp
+++ src/Simulation2.cpp
@@ -34,12 +34,12 @@ struct Particle {
 };
 
 float rand2() {
-  return (float) rand() / (float) RAND_MAX;
+	return (float) rand() / (float) RAND_MAX;
 }
 
 int main() {
 	srand(time(0));
-	
+
 	Particle myParticles[NUM_PARTICLES];
 	for (int x = 0; x < NUM_PARTICLES; ++x) {
 		myParticles[x].pos = make_float3(rand2() * 2.0 - 1.0, rand2() * 2.0 - 1.0, rand2() * 2.0 - 1.0) * UNIVERSIZE;
@@ -52,23 +52,31 @@ int main() {
 
 	ofstream myfile("myfile.csv");
 	if (!myfile.is_open()) {
-	  cout << "Unable to open." << endl;
-	  return -1;
+		cout << "Unable to open." << endl;
+		return -1;
 	}
-	
+
 	for (int t = 0; t < TIME; ++t) {
-	  for (int t2 = 0; t2 < TIMESTEPS; ++t2) {
-		  for (int x = 0; x < NUM_PARTICLES; ++x) {
-		    for (int y = 0; y < NUM_PARTICLES; ++y) {
-          if (x != y) {
-            
-          }
-        }
+		for (int t2 = 0; t2 < TIMESTEPS; ++t2) {
+			for (int x = 0; x < NUM_PARTICLES; ++x) {
+				for (int y = 0; y < NUM_PARTICLES; ++y) {
+					if (x != y) {
+						float distance = length(myParticles[y].pos - myParticles[x].pos);
+						float gravity = GRAV_CONSTANT * MASS * 2.0 / pow(distance, 2);
+						myParticles[x].frc += gravity * (myParticles[y].pos - myParticles[x].pos) / distance;
+					}
+				}
+			}
+
+			for(int x = 0; x < NUM_PARTICLES; ++x) {
+				myParticles[x].acl = myParticles[x].frc / myParticles[x].mass;
+				myParticles[x].pos += myParticles[x].vel * TIME + 0.5 * myParticles[x].acl * pow(1.0 / TIMESTEPS, 2);
+				myParticles[x].vel += myParticles[x].vel + myParticles[x].acl * 1.0 / TIMESTEPS;
 			}
 		}
-		
+
 		for (int i = 0; i < NUM_PARTICLES; ++i) {
-		  myfile << myParticles[i].pos.x << "," << myParticles[i].vel.x << "," << myParticles[i].frc.x << ",";
+			myfile << myParticles[i].pos.x << "," << myParticles[i].vel.x << "," << myParticles[i].frc.x << ",";
 		}
 		myfile << endl;
 	}
